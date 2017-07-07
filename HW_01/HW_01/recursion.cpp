01879284a1c68dc923c33740131e1667838120f1
AllenWang <allenwang950321@gmail.com>
2017-07-06 17:45:24 -0700

    start hw 01

---
+++ HW_01/HW_01/recursion.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Returns the product of two positive integers, m and n,
+// using only repeated addition.
+int mult(unsigned int m, unsigned int n)
+{
+	return -1;  // This is incorrect.
+}
+
+// Returns the number of occurrences of digit in the decimal
+// representation of num. digit is an int between 0 and 9
+// inclusive.
+//
+// Pseudocode Example:
+//    countDigit(18838, 8) => 3
+//    countDigit(55555, 3) => 0
+//    countDigit(0, 0)     => 0
+//
+int countDigit(int num, int digit)
+{
+	return -1;  // This is incorrect.
+}
+
+// Returns a string where the same characters next each other in
+// string n are separated by "++"
+//
+// Pseudocode Example:
+//    pairPlus("goodbye") => "go++odbye"
+//    pairPlus("yyuu")    => "y++yu++u"
+//    pairPlus("aaaa")    => "a++a++a++a"
+//
+string pairPlus(string n)
+{
+	return "";  // This is not always correct.
+}
+
+// str contains a single pair of parenthesis, return a new string
+// made of only the parenthesis and whatever those parensthesis
+// contain.
+//
+//  Pseudocode Example:
+//     subParen("abc(ghj)789") => "(ghj)"
+//     subParen("(x)7")        => "(x)"
+//     subParen("4agh(y)")     => "(y)"
+//
+string subParen(string str)
+{
+	return "*";  // This is incorrect.
+}
+
+// Return true if the sum of any combination of elements in the array
+// a equals the value of the target.
+//
+//  Pseudocode Example:
+//     sumCombination([2, 4, 8], 3, 10) => true
+//     sumCombination([2, 4, 8], 3, 12) => true
+//     sumCombination([2, 4, 8], 3, 11) => false
+//     sumCombination([], 0, 0)         => true
+//
+bool sumCombination(const int a[], int size, int target)
+{
+	return false;  // This is not always correct.
+}
+
+
+int main() {
+
+
+
+
+}
